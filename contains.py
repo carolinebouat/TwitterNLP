ea0749b837db497d79ae985b42c1913c6883de9a
carolinebouat <53908079+carolinebouat@users.noreply.github.com>
2022-01-10 10:26:40 +0100

    Delete contains.py

--- contains.py
+++
@@ -1,6 +0,0 @@
-def contains(original, targets):
-	'''
-	check wether the is a least one occurence of each of the words in 
-	targets in original. This is not case-dependent.
-	'''
-	return any(s in original.lower() for s.lower() in targets)
